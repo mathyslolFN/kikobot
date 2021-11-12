4ae1588557bec758ea2e6f4acddc0976222540a0
mathyslolFN <93182170+mathyslolFN@users.noreply.github.com>
2021-11-12 16:02:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
 import asyncio
 import os
-
-os.system('pip install -U uvloop')
-
-import uvloop
 import sys
-
-if not os.getenv('DEVICE_ID') and \
-        not os.getenv('ACCOUNT_ID') and \
-        not os.getenv('SECRET'):
-    print("Please paste your device auths into the \".env\" file.\n")
-    sys.exit()
-
-os.system('pip install -U SEKKAYBOT')
-os.system('clear')
-
-import SEKKAYBOT
+import uvloop
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-client = SEKKAYBOT.SekkayBot(
-    device_id=os.getenv('DEVICE_ID'),
-    account_id=os.getenv('ACCOUNT_ID'),
-    secret=os.getenv('SECRET')
+os.system('pip install -U XBXBot')
+os.system('clear')
+
+import FNBOT2
+
+client = XBXBot.PartyBot(
+  device_id=os.getenv('DEVICE_ID'),
+  account_id=os.getenv('ACCOUNT_ID'),
+  secret=os.getenv('SECRET')
 )
 
 try:
